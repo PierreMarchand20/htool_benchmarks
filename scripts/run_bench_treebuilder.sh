289b05305ae63856611bc20c40a89f655d5704aa
vdubos <virgile.dubos@proton.me>
2024-04-19 15:02:39 +0200

    Disabling CPU Frequency Scaling i.e. fix warning :  "***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incur extra overhead."

--- scripts/run_bench_treebuilder.sh
+++ scripts/run_bench_treebuilder.sh
@@ -11,13 +11,12 @@ mkdir -p build
 cd build
 # rm bench_hmatrix.csv
 CC=gcc CXX=g++ cmake ../ -DCMAKE_BUILD_TYPE=Release_w_vecto
-make BenchTreeBuilder
+make bench_hmatrix_build
 buildpath=${MY_PATH}/../build_intel
 
 # Output
 mkdir -p ${MY_PATH}/../output
 outputpath=${MY_PATH}/../output/
-
 
 # HPC data
 nprocs=16
@@ -26,12 +25,21 @@ threads=(1 2 4 8 16)
 procs_per_node=16
 
 # Misc datas
-executable=${buildpath}/BenchTreeBuilder
+executable=${buildpath}/bench_hmatrix_build
 time=00:30:00
 
 # Log folder
 mkdir -p ${MY_PATH}/../logs
 logpath=${MY_PATH}/../logs
 
+# change CPU mode to performance BEFORE running benchmarks
+sudo cpupower frequency-set --governor performance > /dev/null
+
 # Scripts
-./BenchTreeBuilder
+./bench_hmatrix_build
+
+# change back CPU mode to powerersave AFTER running benchmarks
+sudo cpupower frequency-set --governor powersave > /dev/null
+
+# check current CPU mode
+# cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
