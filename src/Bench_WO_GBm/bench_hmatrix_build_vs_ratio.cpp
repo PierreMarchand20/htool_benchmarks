9320148194c9b9ec7499250224b01e49e6aa5a7f
vdubos <virgile.dubos@proton.me>
2024-07-26 16:47:59 +0200

    build vs ratio : dim/thread

---
+++ src/Bench_WO_GBm/bench_hmatrix_build_vs_ratio.cpp
@@ -0,0 +1,128 @@
+#include "bench_hmatrix_build.hpp"
+#include "utils.hpp"
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace htool;
+
+// #include <vector>
+// #include <cmath>
+
+std::vector<int> geometric_progression(int a, int r, int n) {
+    // Computes the sequence of n terms of a geometric progression.
+    //
+    // Parameters:
+    // a: The first term of the progression.
+    // r: The common ratio of the progression.
+    // n: The number of terms to compute.
+    //
+    // Returns:
+    // A vector containing the sequence of n terms of the progression.
+    std::vector<int> sequence(n);
+    sequence[0] = a;
+    for (int i = 1; i < n; i++) {
+        sequence[i] = sequence[i - 1] * r;
+    }
+    return sequence;
+}
+
+int main(int argc, char *argv[]) {
+    const int number_of_repetitions = 2;
+    const int number_of_cases       = 5;
+    std::vector<int> List_pbl_size(number_of_cases);
+    std::vector<int> List_thread(number_of_cases);
+    List_pbl_size = geometric_progression(1 << 10, 2, number_of_cases);
+    List_thread   = geometric_progression(1, 2, number_of_cases);
+
+    std::ofstream savefile;
+    savefile.open("bench_hmatrix_build_vs_ratio.csv");
+    savefile << "epsilon, dim_pbl, number_of_threads, algo_type, id_rep, compression_ratio, space_saving, time (s) | mean time (s) | standard_deviation \n";
+
+    for (double epsilon : {1e-10, 1e-8, 1e-6}) {
+        int id_thread = 0;
+        for (int dim_pbl : List_pbl_size) {
+            // Setup
+            FT_Generator fixture;
+            double eta = 10;
+            fixture.SetUp(dim_pbl, dim_pbl, 'N', 'N', epsilon, eta);
+            double list_build_duration[number_of_repetitions] = {0};
+
+            int n_threads = List_thread[id_thread];
+            id_thread++;
+
+            for (string algo_type : {"Classic", "TaskBased"}) {
+                for (int id_rep = 0; id_rep < number_of_repetitions; id_rep++) {
+                    std::chrono::steady_clock::time_point start, end;
+                    omp_set_num_threads(n_threads);
+
+                    if (algo_type == "Classic" || algo_type == "TaskBased") { // Hmatrix
+                        double compression_ratio = 0;
+                        double space_saving      = 0;
+                        if (algo_type == "Classic") {
+                            // Hmatrix
+                            using HMatrixTreeBuilderType = HMatrixTreeBuilder<double, htool::underlying_type<double>>;
+                            std::unique_ptr<HMatrixTreeBuilderType> hmatrix_tree_builder;
+                            hmatrix_tree_builder = std::make_unique<HMatrixTreeBuilderType>(fixture.m_target_root_cluster->get_cluster_on_partition(0), fixture.m_source_root_cluster->get_cluster_on_partition(0), epsilon, eta, 'N', 'N', -1, -1, -1);
+
+                            // Timer
+                            start                                                = std::chrono::steady_clock::now();
+                            auto root_hmatrix                                    = hmatrix_tree_builder->build(*fixture.generator); // *generator
+                            end                                                  = std::chrono::steady_clock::now();
+                            std::chrono::duration<double> classic_build_duration = end - start;
+
+                            // Compression ratio and space saving
+                            auto hmatrix_information = get_hmatrix_information(root_hmatrix);
+                            compression_ratio        = std::stod(hmatrix_information["Compression_ratio"]);
+                            space_saving             = std::stod(hmatrix_information["Space_saving"]);
+
+                            // data saving
+                            savefile << epsilon << ", " << dim_pbl << ", " << n_threads << ", " << algo_type << ", " << id_rep << ", " << compression_ratio << ", " << space_saving << ", " << classic_build_duration.count() << "\n";
+                            list_build_duration[id_rep] = classic_build_duration.count();
+
+                        } else if (algo_type == "TaskBased") {
+                            using HMatrixTreeBuilderType = HMatrixTaskBasedTreeBuilder<double, htool::underlying_type<double>>;
+                            std::unique_ptr<HMatrixTreeBuilderType> hmatrix_tree_builder;
+                            hmatrix_tree_builder = std::make_unique<HMatrixTreeBuilderType>(fixture.m_target_root_cluster->get_cluster_on_partition(0), fixture.m_source_root_cluster->get_cluster_on_partition(0), epsilon, eta, 'N', 'N', -1, -1, -1);
+
+                            // Timer
+                            start                                                   = std::chrono::steady_clock::now();
+                            auto root_hmatrix                                       = hmatrix_tree_builder->build(*fixture.generator); // *generator
+                            end                                                     = std::chrono::steady_clock::now();
+                            std::chrono::duration<double> task_based_build_duration = end - start;
+
+                            // Compression ratio and space saving
+                            auto hmatrix_information = get_hmatrix_information(root_hmatrix);
+                            compression_ratio        = std::stod(hmatrix_information["Compression_ratio"]);
+                            space_saving             = std::stod(hmatrix_information["Space_saving"]);
+
+                            // data saving
+                            savefile << epsilon << ", " << dim_pbl << ", " << n_threads << ", " << algo_type << ", " << id_rep << ", " << compression_ratio << ", " << space_saving << ", " << task_based_build_duration.count() << "\n";
+                            list_build_duration[id_rep] = task_based_build_duration.count();
+                        }
+                        // } else { // Dense. Max dimension pbl = 1 << 15
+                        //     std::vector<double> dense_data(dim_pbl * dim_pbl);
+
+                        //     // Timer
+                        //     start = std::chrono::steady_clock::now();
+                        //     (*fixture.generator).copy_submatrix(dim_pbl, dim_pbl, 0, 0, dense_data.data());
+                        //     end                                                = std::chrono::steady_clock::now();
+                        //     std::chrono::duration<double> dense_build_duration = end - start;
+
+                        //     // data saving
+                        //     savefile << epsilon << ", " << dim_pbl << ", " << n_threads << ", " << algo_type << ", " << id_rep << ", " << 0 << ", " << 0 << ", " << dense_build_duration.count() << "\n";
+                        //     list_build_duration[id_rep] = dense_build_duration.count();
+                    }
+                }
+                // mean and stddev saving
+                double mean, std_dev;
+                compute_standard_deviation(list_build_duration, number_of_repetitions, mean, std_dev);
+                savefile << epsilon << ", " << dim_pbl << ", " << n_threads << ", " << algo_type << ", " << "mean" << ", " << "N.A" << ", " << "N.A" << ", " << mean << "\n";
+                savefile << epsilon << ", " << dim_pbl << ", " << n_threads << ", " << algo_type << ", " << "stddev" << ", " << "N.A" << ", " << "N.A" << ", " << std_dev << "\n";
+            }
+        }
+    }
+    savefile.close();
+    return 0;
+}
---
+++ src/Bench_WO_GBm/plot_bench_vs_ratio.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+import os , time, sys
+import matplotlib.animation as animation
+from math import exp, sqrt, ceil, log10
+import pandas
+import os
+
+def geometric_progression(a, r, length):
+    return [a * r ** (n - 1) for n in range(1, length + 1)]
+
+######################## Parameters #########################################
+
+List_pbl_size          = geometric_progression(1 << 10, 2, 5)
+List_number_of_threads = geometric_progression(1, 2, 5)
+List_implementation    = ["Classic", "TaskBased"]
+List_epsilon           = [1e-10, 1e-8, 1e-6]
+is_log_scale           = False
+plot_result_file       = "/bench_hmatrix_build_vs_ratio.csv"; bench_result_path = "../output/Build_hmatrix_vs_ratio/" # Build_hmatrix_vs_thread
+#############################################################################
+
+# Update bench_result_path with List_pbl_size
+for id_N in range(len(List_pbl_size)):
+    bench_result_path += str(List_pbl_size[id_N]) + "_"
+bench_result_path = bench_result_path.rstrip('_') # Remove last element of bench_result_path
+
+# Create bench_result_path if it doesn't exist
+os.makedirs(bench_result_path, exist_ok=True)
+
+# Import data
+df = pandas.read_csv(bench_result_path+plot_result_file, sep=', ', engine='python') 
+data = 'time (s) | mean time (s) | standard_deviation'
+
+# Variables
+Mat_mean_time = np.zeros((len(List_epsilon), len(List_pbl_size), len(List_implementation)))
+Mat_stddev = np.zeros_like(Mat_mean_time)
+
+for id_BM in range(len(df[:])): 
+    id_epsilon = List_epsilon.index(df['epsilon'][id_BM])
+    id_N       = List_pbl_size.index(df['dim_pbl'][id_BM])
+    id_impl    = List_implementation.index(df['algo_type'][id_BM])
+    
+    if df['id_rep'][id_BM] == 'mean':
+        Mat_mean_time[id_epsilon, id_N, id_impl] = df[data][id_BM]
+    if df['id_rep'][id_BM] == 'stddev':
+        Mat_stddev[id_epsilon, id_N, id_impl] = df[data][id_BM]
+             
+# Plots     
+List_markers = ['o', 's', 'D', 'v', '^', 'h', 'd'] # for epsilon
+List_colors  = ['b', 'r', 'g', 'c', 'm', 'y', 'k'] # for implementation
+          
+# Plot real time
+pltA = plt.subplot(211)
+pltA.set_title("Mean real time of hmatrix building as a function of ratio: " + str(List_pbl_size) + " / " + str(List_number_of_threads))
+pltA.set_ylabel("Real time (s)")
+
+for id_impl in range(len(List_implementation)):
+    for id_epsilon in range(len(List_epsilon)):        
+        pltA.errorbar(List_pbl_size, Mat_mean_time[id_epsilon, :, id_impl], yerr=Mat_stddev[id_epsilon, :, id_impl], fmt=List_markers[id_epsilon]+'-'+List_colors[id_impl], markerfacecolor='none', capsize=3, label=List_implementation[id_impl]+ ", epsilon: "+str(List_epsilon[id_epsilon]))
+        
+if is_log_scale:
+    plt.yscale('log')
+    plt.xscale('log')
+    Array_N = np.array(List_pbl_size)
+    pltA.plot(List_pbl_size, Array_N*np.log10(Array_N) / (Array_N[0]*np.log10(Array_N)[0]) * Mat_mean_time[0, 0, 1], '--', label='NlogN')
+    pltA.plot(List_pbl_size, Array_N*Array_N / (Array_N[0]*Array_N[0]) * Mat_mean_time[0, 0, 1], '--', label='N^2')
+    
+pltA.legend()
+
+# Save and show figure
+mng = plt.get_current_fig_manager()
+mng.resize(*mng.window.maxsize())
+figure = plt.gcf()
+plt.show()
+figure.savefig(bench_result_path+'/mean_time_vs_pbl_size.png', format='png',bbox_inches = "tight") 
+
+# cout
+print("Plotting : " + bench_result_path + plot_result_file)
+print("List_pbl_size : " + str(List_pbl_size))
+print("List_implementation : " + str(List_implementation))
+print("List_epsilon : " + str(List_epsilon))
+print("is_log_scale : " + str(is_log_scale))
