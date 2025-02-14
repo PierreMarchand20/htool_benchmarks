abb52a29aa6ee16fa60055f6da3a5ad9bc96d56c
vdubos <virgile.dubos@proton.me>
2025-02-14 16:00:37 +0100

    add find_L0 to view_block_tree + refactoring

--- include/htool_benchmark/WIP_task_based_hmatrix_factorization.hpp
+++ include/htool_benchmark/WIP_task_based_hmatrix_factorization.hpp
@@ -4,8 +4,8 @@
 namespace htool {
 
 struct TreeCounts {
-    int call_count = 0;
-    // int node_count = 0;
+    size_t call_count = 0;
+    size_t node_count = 0;
 };
 
 template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
@@ -16,7 +16,9 @@ std::size_t cost_function(const HMatrix<CoefficientPrecision, CoordinatePrecisio
 }
 
 template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
-std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> count_nodes(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, const double criterion) { // count the number of nodes in one layer depending on criterion
+std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> count_nodes(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, const double criterion) {
+
+    // count the number of nodes in one layer depending on criterion
     std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> result;
 
     if (cost_function(hmatrix) <= criterion || hmatrix.is_leaf()) {
@@ -32,6 +34,7 @@ std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> count_no
 
 template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
 std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> find_l0(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, const size_t nb_nodes_max) {
+
     // initialize criterion
     double criterion = cost_function(hmatrix);
     std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> old_result, result = count_nodes(hmatrix, criterion);
@@ -41,8 +44,7 @@ std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> find_l0(
         return std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *>();
 
     } else {
-        // dychotomy search for optimal criterion
-
+        // dichotomy search for optimal criterion
         do {
             // save old iteration
             old_result = result;
@@ -51,20 +53,13 @@ std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> find_l0(
             if (all_leaves(result)) {
                 return result;
             }
+
             // update criterion
             criterion /= 2;
+
+            // update result
             result = count_nodes(hmatrix, criterion);
         } while (result.size() <= nb_nodes_max);
-        // {
-        //     // save old iteration
-        //     old_criterion = criterion;
-
-        //     // if nb_nodes_max can't be reached nor surpassed we need to break loop
-        //     if (all_leaves(count_nodes(hmatrix, criterion))) {
-        //         return count_nodes(hmatrix, criterion);
-        //     }
-
-        // }
     }
     return old_result;
 }
@@ -78,20 +73,23 @@ bool all_leaves(const std::vector<const HMatrix<CoefficientPrecision, Coordinate
 
 template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
 void view_block_tree(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix) {
+    // Find L0
+    std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> L0 = find_l0(hmatrix, 64);
 
     // Create a DOT file
     std::ofstream dotFile("block_tree.dot");
 
     // Write the DOT file contents
     dotFile << "digraph {\n";
-    dotFile << "  tooltip=\"block tree\";\n";
 
     // Call the function to create the tree
-    // create_block_tree(hmatrix, dotFile);
     TreeCounts counts;
-    create_block_tree(hmatrix, dotFile, counts);
+    create_block_tree(hmatrix, dotFile, counts, L0);
 
     // End of the DOT file contents
+    dotFile << "  tooltip=\"Block tree information: \\n"
+            << "number of nodes: " << counts.node_count << "\\n"
+            << "number of nodes in L0: " << L0.size() << "\";\n";
     dotFile << "}\n";
 
     // Close the DOT file
@@ -111,70 +109,109 @@ std::string get_hmatrix_id(const HMatrix<CoefficientPrecision, CoordinatePrecisi
     return target_min + "_" + target_max + "_" + source_min + "_" + source_max;
 }
 
-template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
-void create_block_tree(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, std::ostream &dotFile, TreeCounts &counts) {
-    counts.call_count++;
-
-    // Add root
-    if (counts.call_count == 1) {
-        auto hmatrix_info = get_hmatrix_information(hmatrix);
-        dotFile << "    H_" << get_hmatrix_id(hmatrix) << " [tooltip=\"";
-        for (const auto &info : hmatrix_info) {
-            dotFile << info.first << ": " << info.second << "\\n";
-        }
-        dotFile << "\"];\n";
-    }
-
-    // Add child nodes
-    for (const auto &child : hmatrix.get_children()) {
-        auto child_info = get_hmatrix_information(*child.get());
-        dotFile << "    H_" << get_hmatrix_id(*child.get()) << " [tooltip=\"";
-        for (const auto &info : child_info) {
-            dotFile << info.first << ": " << info.second << "\\n";
-        }
-        dotFile << "\"];\n";
-        dotFile << "    H_" << get_hmatrix_id(hmatrix) << " -> H_" << get_hmatrix_id(*child.get()) << ";\n";
-
-        // Recursively add child nodes
-        create_block_tree(*child.get(), dotFile, counts);
-    }
-}
-
 // template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
-// void create_block_tree(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, std::ostream &dotFile, TreeCounts &counts) {
+// void create_block_tree(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, std::ostream &dotFile, TreeCounts &counts, const std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> &L0) {
 //     counts.call_count++;
 
 //     // Add root
 //     if (counts.call_count == 1) {
+//         // Define node
+//         dotFile << "    H_" << get_hmatrix_id(hmatrix) << " [tooltip=\"Node information: \\n";
+
+//         // Add node information
 //         auto hmatrix_info = get_hmatrix_information(hmatrix);
-//         dotFile << "    H0" << " [tooltip=\"";
 //         for (const auto &info : hmatrix_info) {
 //             dotFile << info.first << ": " << info.second << "\\n";
 //         }
-//         dotFile << "\"];\n";
+
+//         // Check if the current node is in L0 and color it in light blue if true
+//         dotFile << "\"";
+//         if (std::find(L0.begin(), L0.end(), &hmatrix) != L0.end()) {
+//             dotFile << ", style=filled, fillcolor=lightblue";
+//         }
+
+//         // End of node definition
+//         dotFile << "];\n";
+
+//         // Increment node count
 //         counts.node_count++;
 //     }
 
 //     // Add child nodes
-//     int local_node_count = 0;
 //     for (const auto &child : hmatrix.get_children()) {
-//         dotFile << "    H" << counts.node_count << " [tooltip=\"";
+//         // Define node
+//         dotFile << "    H_" << get_hmatrix_id(*child.get()) << " [tooltip=\"Node information: \\n";
+
+//         // Add node information
 //         auto child_info = get_hmatrix_information(*child.get());
 //         for (const auto &info : child_info) {
 //             dotFile << info.first << ": " << info.second << "\\n";
 //         }
-//         local_node_count++;
+
+//         // Check if the child node is in L0 and color it in light blue if true
+//         dotFile << "\"";
+//         if (std::find(L0.begin(), L0.end(), child.get()) != L0.end()) {
+//             dotFile << ", style=filled, fillcolor=lightblue";
+//         }
+
+//         // End of node definition
+//         dotFile << "];\n";
+
+//         // Add edge
+//         dotFile << "    H_" << get_hmatrix_id(hmatrix) << " -> H_" << get_hmatrix_id(*child.get()) << " [tooltip=\"" << get_hmatrix_id(hmatrix) << " -> " << get_hmatrix_id(*child.get()) << "\"];\n";
+
+//         // Increment node count
 //         counts.node_count++;
-//         dotFile << "local_node_count = " << local_node_count << "\\n";
-//         dotFile << "node_count = " << counts.node_count << "\\n";
-//         dotFile << "\"];\n";
-//         dotFile << "    H" << counts.call_count - local_node_count << " -> H" << counts.call_count << ";\n";
 
 //         // Recursively add child nodes
-//         create_block_tree(*child.get(), dotFile, counts);
-//         // counts.call_count++;
+//         create_block_tree(*child.get(), dotFile, counts, L0);
 //     }
 // }
+
+template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
+void add_node_to_block_tree(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, std::ostream &dotFile, TreeCounts &counts, const std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> &L0) {
+    // Define node
+    dotFile << "    H_" << get_hmatrix_id(hmatrix) << " [tooltip=\"Node information: \\n";
+
+    // Add node information
+    auto hmatrix_info = get_hmatrix_information(hmatrix);
+    for (const auto &info : hmatrix_info) {
+        dotFile << info.first << ": " << info.second << "\\n";
+    }
+
+    // Check if the current node is in L0 and color it in light blue if true
+    dotFile << "\"";
+    if (std::find(L0.begin(), L0.end(), &hmatrix) != L0.end()) {
+        dotFile << ", style=filled, fillcolor=lightblue";
+    }
+
+    // End of node definition
+    dotFile << "];\n";
+
+    // Increment node count
+    counts.node_count++;
+}
+
+template <typename CoefficientPrecision, typename CoordinatePrecision = underlying_type<CoefficientPrecision>>
+void create_block_tree(const HMatrix<CoefficientPrecision, CoordinatePrecision> &hmatrix, std::ostream &dotFile, TreeCounts &counts, const std::vector<const HMatrix<CoefficientPrecision, CoordinatePrecision> *> &L0) {
+    counts.call_count++;
+
+    // Add root
+    if (counts.call_count == 1) {
+        add_node_to_block_tree(hmatrix, dotFile, counts, L0);
+    }
+
+    // Add child nodes
+    for (const auto &child : hmatrix.get_children()) {
+        add_node_to_block_tree(*child.get(), dotFile, counts, L0);
+
+        // Add edge
+        dotFile << "    H_" << get_hmatrix_id(hmatrix) << " -> H_" << get_hmatrix_id(*child.get()) << " [tooltip=\"" << get_hmatrix_id(hmatrix) << " -> " << get_hmatrix_id(*child.get()) << "\"];\n";
+
+        // Recursively add child nodes
+        create_block_tree(*child.get(), dotFile, counts, L0);
+    }
+}
 
 } // namespace htool
 
